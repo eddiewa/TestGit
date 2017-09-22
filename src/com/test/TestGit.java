d427748ec5500e8f46f9ec77219c8672d58a84b9
eddie <pipigo40229@hotmail.com>
2017-09-22 15:35:39 +0800

    add new project test

---
+++ src/com/test/TestGit.java
@@ -0,0 +1,10 @@
+package com.test;
+
+public class TestGit {
+
+	public static void main(String[] args) {
+		System.out.println("hello git to big mama");
+		System.out.println("i like apple");
+	}
+
+}
