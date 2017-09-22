faefdc105a6e65b7276d12666af1428b0b4047dd
eddie <pipigo40229@hotmail.com>
2017-09-22 17:10:16 +0800

    test2...2

--- src/com/test/TestGit2.java
+++ src/com/test/TestGit2.java
@@ -3,9 +3,7 @@ package com.test;
 public class TestGit2 {
 
 	public static void main(String[] args) {
-		System.out.println("i am TestGit2");
+		System.out.println("i am Test2");
 		System.out.println("i like banala");
-		System.out.println("don't be afriend");
 	}
-
 }
