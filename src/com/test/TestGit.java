8a3d1db030a4894a1bb9a867b51e4a353f0a623a
eddie <pipigo40229@hotmail.com>
2017-09-22 15:39:09 +0800

    chenage in branch_1

--- src/com/test/TestGit.java
+++ src/com/test/TestGit.java
@@ -3,7 +3,7 @@ package com.test;
 public class TestGit {
 
 	public static void main(String[] args) {
-		System.out.println("hello git to big mama");
+		System.out.println("test branch_1");
 		System.out.println("i like apple");
 	}
 
