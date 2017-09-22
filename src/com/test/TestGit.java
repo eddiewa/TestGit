46f86c5a8eae4b75e30c6bb1c8ef6f6aefa0538d
eddie <pipigo40229@hotmail.com>
2017-09-22 17:08:05 +0800

    test1...1

--- src/com/test/TestGit.java
+++ src/com/test/TestGit.java
@@ -3,7 +3,7 @@ package com.test;
 public class TestGit {
 
 	public static void main(String[] args) {
-		System.out.println("test branch_2");
+		System.out.println("test 1");
 		System.out.println("i like pineApple!!!!");
 	}
 
